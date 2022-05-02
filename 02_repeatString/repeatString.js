8f0865c4e5f35ffab73f9f2a5ee34860ac10079c
Bzparkes <bzparkes@yahoo.com.au>
2022-05-02 18:56:32 +1000

    Supposed to be easy but it's pretty hard without re-reading the learning material multiple times. Doesn't explain it very well but this makes more sense now it's compelted. Need to go back and re-read yet again, I think

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,15 @@
-const repeatString = function() {
 
-};
+const repeatString = function(word,num) {
+   if(num < 3) return 'ERROR';
+   let string = '';
+   for (let i =0; i < num; i++) {
+       string += word;
+   }
+   return string;
+}
 
+
+
+repeatString()
 // Do not edit below this line
 module.exports = repeatString;
