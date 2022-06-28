e9196f0c5310eee2800eef372c4f7f9aa9104984
Bzparkes <bzparkes@yahoo.com.au>
2022-06-28 14:12:32 +1000

    Still working my way through this exercise. Links don't explain much...

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,13 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(...args) {
+    return args.filter((e) => {
+        return e;
+    })
+    
 };
+
+
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
