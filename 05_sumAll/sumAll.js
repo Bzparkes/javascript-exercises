c6519349f0cfdcb9743c4d546655341efbf60d4d
Bzparkes <bzparkes@yahoo.com.au>
2022-06-28 20:21:53 +1000

    More research required on this one. Don't think I'm TOO far off...

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,16 @@
-const sumAll = function() {
+const sumAll = function(...args) {
+    const array = [];
+    let sum = args;
 
+    for(let i = 0; i < array.length; i++){
+        sum += array[i];
+        
+    }
+    console.log(sum)
+    return sum;
 };
+
+sumAll();
 
 // Do not edit below this line
 module.exports = sumAll;
