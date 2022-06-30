535f3f2e8518102b7223e0aab4ee3fa4e9bc457e
Bzparkes <bzparkes@yahoo.com.au>
2022-06-30 14:06:33 +1000

    Leapyears exercises

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,14 @@
-const leapYears = function() {
-
+const leapYears = function(years) {
+    let yearCheck = 2022;
+    for(let i = 0; i < yearCheck; i++)
+    console.log(yearCheck)
+    if (isLeapYear === 2) {
+        //return isLeapYear;
+        console.log(isLeapYear)
+    }
+    
 };
+leapYears()
 
 // Do not edit below this line
 module.exports = leapYears;
